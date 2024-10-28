2ad0d5f8380147298967389a9326eebfb03da46e
ink3rroll <ink3rroll@gmail.com>
2024-10-28 09:59:55 +0800

    display college names done

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -4,21 +4,38 @@ from django.contrib import admin
 from .models import College, Program, Organization, Student, OrgMember
 
 admin.site.register(College)
-admin.site.register(Program)
-admin.site.register(Organization)
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ("prog_name", "college",)
+
+    search_fields = ("prog_name",)
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("name", "college",)
+
+    search_fields = ("name",)
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ("student_id", "lastname", "firstname", "middlename", "program")
+    list_display = ("student_id", "lastname", "firstname", "middlename", "program", "college",)
 
     search_fields = ("lastname", "firstname",)
 
+    def college(self, obj):
+        try:
+            prog = Program.objects.get(prog_name=obj.program)
+            return prog.college
+        
+        except Program.DoesNotExist:
+            return None
+
 @admin.register(OrgMember)
 class OrgMemberAdmin(admin.ModelAdmin):
-    list_display = ("student", "get_member_program", "organization", "date_joined",)
+    list_display = ("student", "program", "organization", "date_joined",)
 
     search_fields = ("student_lastname", "student_firstname",)
 
-    def get_member_program(self, obj):
+    def program(self, obj):
         try:
             member = Student.objects.get(id=obj.student_id)
             return member.program
