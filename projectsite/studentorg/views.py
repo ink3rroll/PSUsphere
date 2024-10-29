d0fb20066ffacdf0989e6ea655741261f81c6fdc
ocelotmannn <ronaldkun999@gmail.com>
2024-10-29 19:55:13 +0800

    finished organization list features

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from studentorg.models import Organization
 from studentorg.forms import OrganizationForm
 from django.urls import reverse_lazy 
+
+class OrganizationDeleteView(DeleteView):
+    model = Organization
+    template_name = 'org_del.html'
+    success_url = reverse_lazy('organization-list')
+
 
 class OrganizationUpdateView(UpdateView):
     model = Organization
